355a76bba08ef4b6ff7c68d23830754f2e91bbb4
Lakiya <chirantha7@gmail.com>
2026-06-24 12:05:27 +0530

    feat: register Wedding Book in template registry

--- tap-invite/src/app/features/templates/template-registry.ts
+++ tap-invite/src/app/features/templates/template-registry.ts
@@ -3,11 +3,13 @@ import { TemplateManifest } from './template.types';
 import { defaultMinimalManifest } from './default-minimal/default-minimal.manifest';
 import { softFloralManifest }     from './soft-floral/soft-floral.manifest';
 import { flipCardManifest }        from './flip-card/flip-card.manifest';
+import { weddingBookManifest }     from './wedding-book/wedding-book.manifest';
 
 export const TEMPLATE_REGISTRY: TemplateManifest[] = [
   defaultMinimalManifest,
   softFloralManifest,
   flipCardManifest,
+  weddingBookManifest,
 ];
 
 export function getManifest(id: string): TemplateManifest {
